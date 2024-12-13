8b4977548ee6674070d5ada2ae680627fb40318d
Wilfredery <wilfredery30@gmail.com>
2024-12-12 16:19:38 -0800

    Adding perfomance codes

--- src/js/js.js
+++ src/js/js.js
@@ -22,9 +22,13 @@ function createGallery() {
     const gallery = document.querySelector('.galeria-imagen');
 
     for(let i = 1; i <= 16; i++) {
-        const imagen = document.createElement('IMG');
-        imagen.src = `src/img/gallery/full/${i}.jpg`;
-        imagen.alt = 'imagengaleria';
+        const imagen = document.createElement('PICTURE');
+        imagen.innerHTML = `
+            <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+            <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+    `;
+
         imagen.classList.add('imagen');
         gallery.appendChild(imagen);
 
@@ -36,9 +40,12 @@ function createGallery() {
 }
 
 function mostrarimagen(i) {
-    const imagen = document.createElement('IMG');
-    imagen.src = `src/img/gallery/full/${i}.jpg`;
-    imagen.alt = 'imagengaleria';
+    const imagen = document.createElement('PICTURE');
+    imagen.innerHTML = `
+        <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+    `;s
     //Generando el modal
     const modal = document.createElement('DIV');
     modal.classList.add('modal');
